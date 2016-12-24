70af27ea646c5318f5701b59eb5e00cc271bd4ba
honest <honestold3@gmail.com>
2016-12-24 10:04:59 +0800

    es test

---
+++ src/main/scala/com/finupgroup/cif/elasticsearch/EsDemo.scala
@@ -0,0 +1,34 @@
+package com.finupgroup.cif.elasticsearch
+
+import org.elasticsearch.spark._
+import org.elasticsearch.spark.sql._
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql.SQLContext
+
+
+/**
+  * Created by wq on 2016/12/23.
+  */
+object EsDemo {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .set("es.nodes","honest")
+      .set("es.port","9200")
+      .setMaster("spark://honest:7077")
+      .setAppName("es_hdfs")
+
+    val sc = new SparkContext(conf)
+    val sqlContext = SQLContext.getOrCreate(sc)
+    import sqlContext.implicits._
+    //查询合作方为abc的数据
+    val query = """{"query":{"match":{"activity.partnerCode": "abc"}}}"""
+
+    //将在es中的查询结果转化为rdd/dataFrame
+    val esRdd = sc.esRDD(s"index/type",query)
+    //直接读入全部数据
+    val esDf = sqlContext.esDF(s"index/type")
+  }
+
+}
---
+++ src/main/scala/com/finupgroup/cif/elasticsearch/EsDomeTest.scala
@@ -0,0 +1,8 @@
+package com.finupgroup.cif.elasticsearch
+
+/**
+  * Created by wq on 2016/12/24.
+  */
+object EsDomeTest {
+
+}
