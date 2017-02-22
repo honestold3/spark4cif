092ed3b427a91f8a35b59f626ad802866b1f3cff
honestold3 <honestold3@gmail.com>
2017-02-22 10:11:36 +0800

    save elasticsearch

---
+++ src/main/scala/com/finupgroup/cif/elasticsearch/EsNew.scala
@@ -0,0 +1,32 @@
+package com.finupgroup.cif.elasticsearch
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+import org.elasticsearch.spark._
+import com.puhui.aes.AesEncryptionUtil
+
+/**
+  * Created by wq on 2017/2/21.
+  */
+object EsNew {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .set("es.nodes",args(0))
+      .set("es.port",args(1))
+      .setAppName("es_upload")
+
+    val sc = new SparkContext(conf)
+
+    sc.objectFile[scala.collection.immutable.HashMap[String,String]]("/es").foreach{x=>
+      println(x)
+      println(x.getOrElse("biz","wwwww"))
+      println(x.isInstanceOf[Map[String,String]])
+      println("###############")
+    }
+
+//    sc.objectFile[scala.collection.immutable.HashMap[String,String]]("/es")
+//      .saveToEs(s"cif/indexFeatureToHbaseRowkey",Map("es.mapping.id"->"id"))
+  }
+
+}
